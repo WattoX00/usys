601dfbaa97f13475dd610c4cb4c10fd50bb587c0
wattox00 <korschzozinho@gmail.com>
2026-02-13 17:08:02 +0100

    useradd bug fix

--- app/main.py
+++ app/main.py
@@ -1,37 +1,38 @@
 import subprocess
 
-
 def userAdd():
-    newUserName = str(input('New User Name: '))
-    # Home Directory
-    homeQ = str(input(f'Do you want to add home directory [home/{newUserName}] (Y/n)')).lower()
-    homeDir = ''
-    if homeQ in ('y','yes',''):
-        homeDir = '-m'
-        
-    groupQ = str(input(f'Do you want to add ({newUserName}) to a group (Y/n)')).lower()
-    groupName = ''
-    if groupQ in ('y','yes',''):
-        listGroups()
-        groupName = str(input('Type the primary group first then space them with space ( ) for secondary groups ')).lower()
-    if len(groupName.split(' ')) > 1:
-        list_of_groups = groupName.split(' ')
-        first_group = '-g '+list_of_groups[0]
-        list_of_groups.pop(0)
-        secondary_groups = '-G '+",".join(list_of_groups)
-    elif len(groupName.split(' ')) == 1:
-        frist_group = '-g '+groupName
-        secondary_groups = ''
-    else:
-        first_group = ''
-        secondary_groups = ''
-    subprocess.run(["bash", "-c", f"sudo useradd {homeDir} {first_group} {secondary_groups} {newUserName}"])
+    newUserName = input('New User Name: ').strip()
 
-#subprocess.run(["bash", "-c", "echo hello world"])
+    cmd = ["sudo", "useradd"]
+
+    # Home directory
+    homeQ = input(f'Do you want to add home directory [home/{newUserName}] (Y/n) ').lower()
+    if homeQ in ("y", "yes", ""):
+        cmd.append("-m")
+
+    # Groups
+    groupQ = input(f'Do you want to add ({newUserName}) to a group (Y/n) ').lower()
+    if groupQ in ("y", "yes", ""):
+        listGroups()
+        groupName = input('Primary group first, then secondary groups separated by space: ').lower()
+        groups = groupName.split()
+
+        if len(groups) >= 1:
+            cmd += ["-g", groups[0]]
+
+        if len(groups) > 1:
+            cmd += ["-G", ",".join(groups[1:])]
+
+    cmd.append(newUserName)
+
+    subprocess.run(cmd)
 
 def listGroups():
-    subprocess.run(["bash", "-c", "getent group | awk -F: '$3 >= 1000 || $1 ~ /^(sudo|wheel|docker)$/ {print $1}'"])
-
+    subprocess.run([
+        "bash",
+        "-c",
+        "getent group | awk -F: '$3 >= 1000 || $1 ~ /^(sudo|wheel|docker)$/ {print $1}'"
+    ])
 
 userAdd()
 
