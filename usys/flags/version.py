70ab37ab9109b317e8b08f3ec7ad35a20139f4e4
wattox00 <korschzozinho@gmail.com>
2026-03-08 17:09:35 +0100

    Version flag added

---
+++ usys/flags/version.py
@@ -0,0 +1,26 @@
+import sys
+import requests
+from importlib.metadata import version, PackageNotFoundError
+
+PACKAGE_NAME = "usys"
+
+class Version():
+    def version():
+        try:
+            installed_version = version(PACKAGE_NAME)
+        except PackageNotFoundError:
+            installed_version = "not installed"
+
+        try:
+            resp = requests.get(f"https://pypi.org/pypi/{PACKAGE_NAME}/json", timeout=3)
+            resp.raise_for_status()
+            latest_version = resp.json()["info"]["version"]
+        except Exception:
+            latest_version = "unknown"
+
+        if installed_version == latest_version:
+            return (f"{PACKAGE_NAME} {installed_version} LTS")
+        else:
+            return (f"{PACKAGE_NAME} {installed_version} newer version available: {latest_version}")
+
+        sys.exit(0)
